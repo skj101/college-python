7b2bc29598c2e0dd554babba01ab384455c8556b
skj101 <144668595+skj101@users.noreply.github.com>
2023-10-16 23:03:56 +0530

    Update 6.py CO1

--- CO1/6.py
+++ CO1/6.py
@@ -1,16 +1,8 @@
 # Sanu K Joseph
-def compare():
-    list1 = list(map(int, input("Enter the first list: ").split()))
-    list2 = list(map(int, input("Enter the second list: ").split()))
-    same_length = len(list1) == len(list2)
-    sum1 = sum(list1)
-    sum2 = sum(list2)
-    same_sum = sum1 == sum2
-    any_common = any(item in list1 for item in list2)
-    return same_length, sum1, sum2, same_sum, any_common
-result = compare()
-print(f"Same length: {result[0]}")
-print(f"Sum of first list: {result[1]}")
-print(f"Sum of second list: {result[2]}")
-print(f"Lists sum to the same value: {result[3]}")
-print(f"Common value in both lists: {result[4]}")
+list1 = list(map(int, input("Enter the first list: ").split()))
+list2 = list(map(int, input("Enter the second list: ").split()))
+print(f"Same length: {len(list1) == len(list2)}")
+print(f"Sum of first list: {sum(list1)}")
+print(f"Sum of second list: {sum(list2)}")
+print(f"Lists sum to the same value: {sum(list1) == sum(list2)}")
+print(f"Common value in both lists: {any(item in list1 for item in list2)}")
