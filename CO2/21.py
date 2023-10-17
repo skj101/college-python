d0217b8e6f7c3e04638551cc669b9cc6b667ff16
skj101 <144668595+skj101@users.noreply.github.com>
2023-10-17 08:22:23 +0530

    Update 21.py CO2

--- CO2/21.py
+++ CO2/21.py
@@ -1,10 +1,5 @@
 # Sanu K Joseph
-def Fib(n):
-   if n <= 1:
-       return n
-   else:
-       return (Fib(n - 1) + Fib(n - 2))
-
-for i in range(int(input("Enter the Value of n: "))):
-   print(Fib(i),end = " ")
-print()
+n = int(input("Enter the number of terms: "))
+f = [0, 1]
+while len(f) < n: f.append(f[-1] + f[-2])
+print(f"Fibonacci series of {n} terms:", f)
