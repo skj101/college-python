6e856c3720099c48712ce6636d768c6232daf2c4
skj101 <144668595+skj101@users.noreply.github.com>
2023-10-17 07:32:57 +0530

    Update 10.py CO1

--- CO1/10.py
+++ CO1/10.py
@@ -1,10 +1,3 @@
 # Sanu K Joseph
-a= int(input("enter the fist number"))
-b= int(input("enter the second number"))
-c= int(input("enter the third number"))
-if (a>=b) and (b>=c):
-	print(a,"is greater")
-elif (b>=a ) and (b>=c) :
-	print(b,"is greater")
-else :
-	print(c,"is greater")
+a, b, c = map(int, input("Enter three numbers separated by spaces: ").split())
+print("The largest number is:",  max(a, b, c))
