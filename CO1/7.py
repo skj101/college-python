a4e714f424f5ad8101c65b9f595316e0c6d0168c
skj101 <144668595+skj101@users.noreply.github.com>
2023-10-17 07:21:06 +0530

    Update 7.py CO1

--- CO1/7.py
+++ CO1/7.py
@@ -1,4 +1,2 @@
-# Sanu K Joseph
-x1 = str(input("Enter a string : "))
-firstchar = x1[0]
-print(firstchar + x1.replace(firstchar, "$")[1:])
+user_input = input("Enter a string: ")
+print("Modified string:", user_input[0] + user_input[1:].replace(user_input[0], '$'))
