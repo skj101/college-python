db52236fc6aff1f0aceb8c9e376b8ec51c9f4934
skj101 <144668595+skj101@users.noreply.github.com>
2023-10-17 07:08:02 +0530

    Update 5.py CO1

--- CO1/5.py
+++ CO1/5.py
@@ -1,4 +1,2 @@
 # Sanu K Joseph
-count_a_in_names = lambda names: sum(name.lower().count('a') for name in names)
-names = input("Enter a list of names : ").split()
-print(f"Total occurrences of 'a': {count_a_in_names(names)}")
+print("Total occurrences of 'a':", sum(name.lower().count('a') for name in input("Enter a list of names : ").split()))
