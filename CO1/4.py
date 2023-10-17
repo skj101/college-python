268495a284fb78497d4a217b45a79db00393d419
skj101 <144668595+skj101@users.noreply.github.com>
2023-10-17 07:02:03 +0530

    Update 4.py CO1

--- CO1/4.py
+++ CO1/4.py
@@ -1,4 +1,2 @@
 # Sanu K Joseph
-get_numbers = lambda: ["over" if int(x) > 100 else int(x) for x in input("Enter numbers separated by spaces: ").split()]
-a = get_numbers()
-print(a)
+print(["over" if int(x) > 100 else int(x) for x in input("Enter numbers separated by spaces: ").split()])
